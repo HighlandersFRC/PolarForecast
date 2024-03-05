0f7cf011118c7fc759649c0ed87858deb09d3242
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2024-03-05 14:55:05 -0700

    added clamping to text input

--- APP/src/components/Counter.js
+++ APP/src/components/Counter.js
@@ -7,7 +7,7 @@ const Counter = ({ label, onChange, value, max}) => {
   const [count, setCount] = useState(value);
 
   const handleChange = (event) => {
-    const newValue = Math.max(Number(event.target.value), 0);
+    const newValue = Math.max(min(Number(event.target.value), max), 0);
     onChange(newValue);
     setCount(newValue);
   };
