2237525efa8d6e92bb32e29348ec4cd5dd9c9af7
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2024-10-01 21:58:41 -0600

    Made a few fixes for KCMT Release

--- APP/src/components/AutosDisplay.js
+++ APP/src/components/AutosDisplay.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { ImageListItemBar, ImageListItem } from '@mui/material';
 
 const AutoDisplay = ({ scoutingData }) => {
     const url = new URL(window.location.href);
@@ -34,38 +35,40 @@ const AutoDisplay = ({ scoutingData }) => {
     const pieceY = [850, 850, 850, 61, 61, 61, 61, 61]
 
     return (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-            <p className="text-white mb-0">{params.length == 5 ? `Team: ${scoutingData.team_number} |` : ""}  Match: {scoutingData.match_number}  |  {params.length == 6 && params[5]?.split("-")[0] == "team" ? `Scout: ${scoutingData.scout_info.name} |` : ""} Scored: {scoutingData.data.auto.amp + scoutingData.data.auto.speaker}</p>
-            <div style={{ position: 'relative', maxWidth: '100%', height: 'auto' }}>
-                {/* Display your field image here */}
-                <img
-                    ref={fieldImageRef}
-                    src={serverPath + "/BlankAutoField.png"}
-                    alt="Field"
-                    style={{ maxWidth: '100%', height: 'auto' }}
-                    onLoad={handleImageLoad}
-                />
-                {pieces.map((piece, idx) => {
-                    return (
-                        <div
-                            key={piece}
-                            style={{
-                                position: 'absolute',
-                                ...calculatePosition(pieceX[idx], pieceY[idx]),
-                                width: `${NOTE_SIZE * imageScaleFactor}px`,
-                                height: `${NOTE_SIZE * imageScaleFactor}px`,
-                                filter: scoutingData.data.selectedPieces.includes(piece) ? 'none' : 'grayscale(100%)',
-                            }}
-                        >
-                            <img src="/Note.png" alt={`Note ${piece}`} style={{ width: '100%', height: '100%' }} />
-                            <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', fontSize: `${100*imageScaleFactor}px`, fontWeight: 'bold'}}>
-                                {scoutingData.data.selectedPieces.indexOf(piece) === -1 ? "" : scoutingData.data.selectedPieces.indexOf(piece) + 1}
-                            </span>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
+        <ImageListItem style={{ display: "flex", flexDirection: "column" }}>
+            <>
+                <div style={{ position: 'relative', maxWidth: '100%', height: 'auto' }}>
+                    {/* Display your field image here */}
+                    <img
+                        ref={fieldImageRef}
+                        src={serverPath + "/BlankAutoField.png"}
+                        alt="Field"
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                        onLoad={handleImageLoad}
+                    />
+                    {pieces.map((piece, idx) => {
+                        return (
+                            <div
+                                key={piece}
+                                style={{
+                                    position: 'absolute',
+                                    ...calculatePosition(pieceX[idx], pieceY[idx]),
+                                    width: `${NOTE_SIZE * imageScaleFactor}px`,
+                                    height: `${NOTE_SIZE * imageScaleFactor}px`,
+                                    filter: scoutingData.data.selectedPieces.includes(piece) ? 'none' : 'grayscale(100%)',
+                                }}
+                            >
+                                <img src="/Note.png" alt={`Note ${piece}`} style={{ width: '100%', height: '100%' }} />
+                                <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', fontSize: `${100 * imageScaleFactor}px`, fontWeight: 'bold' }}>
+                                    {scoutingData.data.selectedPieces.indexOf(piece) === -1 ? "" : scoutingData.data.selectedPieces.indexOf(piece) + 1}
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+            </>
+            <ImageListItemBar position="below" title={(params.length == 5 ? `Team: ${scoutingData.team_number} | ` : "") + "Match:" + scoutingData.match_number + " | " + (params.length == 6 && params[5]?.split("-")[0] == "team" ? `Scout: ${scoutingData.scout_info.name} | ` : "") + "Scored:" + (scoutingData.data.auto.amp + scoutingData.data.auto.speaker)} />
+        </ImageListItem>
     );
 };
 
