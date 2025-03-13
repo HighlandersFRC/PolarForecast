964425a2134f83bdd289c419da338f1adf29bf9a
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2025-03-13 16:13:12 -0600

    added stuff which allegedly fixes rotation stuff

--- APP/lib/main.dart
+++ APP/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:provider/provider.dart';
 import 'package:scouting_app/auth/auth_service.dart';
@@ -17,6 +18,13 @@ import 'api_service.dart'; // Make sure this file contains the ApiService class
 
 Future main() async {
   // await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   usePathUrlStrategy();
   runApp(MyApp());
 }
