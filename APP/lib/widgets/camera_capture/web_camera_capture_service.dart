90dbe68d2739ce14ee0581e87b155a1adff933e3
Thebest-Fish-Programmer <engrdkg@gmail.com>
2026-04-09 11:15:02 -0600

    added back in logic for taking pictures

--- APP/lib/widgets/camera_capture/web_camera_capture_service.dart
+++ APP/lib/widgets/camera_capture/web_camera_capture_service.dart
@@ -1,18 +1,25 @@
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
-import 'camera_capture_service.dart';
 
-class WebCameraCaptureService implements CameraCaptureService {
+import 'package:flutter/material.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
+import 'package:scouting_app/widgets/camera_capture/camera_capture_service.dart';
+
+class WebCameraCaptureService extends CameraCaptureService {
   @override
   Future<Uint8List?> getImage(BuildContext context) async {
-    // your web logic here
-    return null;
+    ImagePickerPlugin imagePicker = ImagePickerPlugin();
+    return await (await imagePicker.getImageFromSource(
+            source: ImageSource.camera))
+        ?.readAsBytes();
   }
 
   @override
   Future<Uint8List?> pickImageFromGallery(BuildContext context) async {
-    // your web logic here
-    return null;
+    ImagePickerPlugin imagePicker = ImagePickerPlugin();
+    return await (await imagePicker.getImageFromSource(
+            source: ImageSource.gallery))
+        ?.readAsBytes();
   }
 }
 
